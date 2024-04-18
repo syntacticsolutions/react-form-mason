f351e116c97db2d51f522a3fd6f764bbd41352d8
Miguel Coder <syntacticsolutions@gmail.com>
2024-04-17 18:32:43 -0700

    Add more validators

--- src/hooks/useFormErrors/validators.ts
+++ src/hooks/useFormErrors/validators.ts
@@ -2,5 +2,23 @@ import _ from "lodash";
 import { ValidatorFuncParams } from "./types";
 
 export const isNumber = ({ value }: ValidatorFuncParams) => {
-  if (!_.isNumber(value)) return '{{label}} is not a number';
+  if (!_.isNumber(value)) return "{{label}} is not a number";
+};
+
+export const maxLength =
+  (length: number) =>
+  ({ value }: ValidatorFuncParams) => {
+    if (value?.length || 0 < length)
+      return `{{label}} cannot be longer than ${length} characters long.`;
+  };
+
+export const minLength =
+  (length: number) =>
+  ({ value }: ValidatorFuncParams) => {
+    if (value?.length || 0 > length)
+      return `{{label}} must be a minimum of ${length} characters long.`;
+  };
+
+export const required = ({ value }: ValidatorFuncParams) => {
+  if (!!value || !value.length) return `{{label}} is required.`;
 };
