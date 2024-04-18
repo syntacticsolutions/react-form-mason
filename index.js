9af367e79aa80e7983b15fd7dfc21373844b8650
Miguel Coder <syntacticsolutions@gmail.com>
2024-04-17 18:54:00 -0700

    Add entrypoint

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Button } = require("./dist/components/Button/Button");
+const { Input } = require("./dist/components/Input/Input");
+const { Select } = require("./dist/components/Select/Select");
+const { Dropdown } = require("./dist/components/Dropdown/Dropdown");
+const {
+  FormGenerator,
+} = require("./dist/components/FormGenerator/FormGenerator");
+const { useFormErrors } = require("./dist/hooks/useFormErrors/useFormErrors");
+const validators = require("./dist/hooks/useFormErrors/validators");
+const { useFormData } = require("./dist/hooks/useFormData");
+
+export default {
+  Button,
+  Input,
+  Select,
+  Dropdown,
+  FormGenerator,
+  useFormErrors,
+  validators,
+  useFormData,
+};
